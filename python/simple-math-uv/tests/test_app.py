86e59309568d5f1333b98e18a1109e0b8ccac540
Alexander Serdyukov <alexander_serdyukov@trimble.com>
2026-05-05 12:49:55 -0600

    Initial commit

---
+++ python/simple-math-uv/tests/test_app.py
@@ -0,0 +1,11 @@
+# test_app.py
+from simple_math_uv.app import add_numbers, subtract_numbers
+
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+
+
+def test_subtract_numbers():
+    assert subtract_numbers(10, 5) == 5
